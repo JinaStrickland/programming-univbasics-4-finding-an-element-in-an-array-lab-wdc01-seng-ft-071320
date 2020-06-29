944b234bb3e9a7ac37922a9ad9d23e00a60685d6
Jina Strickland <jina30@gmail.com>
2020-06-29 21:25:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 
 def find_element_index(array, value_to_find)
   count = 0
-  if array[count] < array.length
-    puts array[count]
+  if count < array.length
+    puts array[count] == value_to_find
     count += 1
   end
 end
